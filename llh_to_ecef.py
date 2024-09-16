e6013cad608c51bf43845f170950d32677cb6769
maxoross <moross8228@vt.edu>
2024-09-16 14:10:32 -0400

    llh_to_ecef.py

--- llh_to_ecef.py
+++ llh_to_ecef.py
@@ -1,30 +1,32 @@
 # llh_to_ecef.py
 #
-# Usage: python3 llh_to_ecef.py lat_deg, lon_deg, hae_km
+# Usage: python3 llh_to_ecef.py lat_deg lon_deg hae_km
 # Description: Converts LLH coordinates to ECEF coordinates.
 #
 # Parameters:
 #  lat_deg: Latitude given in degrees
 #  lon_deg: Longitude given in degrees
-#  hae_km: HAE given in kilometers
+#  hae_km: Altitude given in kilometers
 #
 # Output:
-#  The ECEF coordinates (r_x, r_y, r_z) in kilometers
+#  The ECEF coordinates (x, y, z) in kilometers
 #
 # Written by Maxwell Oross
 # Other contributors: None
+#
+# Optional license statement, e.g., See the LICENSE file for the license.
 
 import math
 import sys
 
 # Constants
 R_E_KM = 6378.137
-E_E = 0.081819221456
+e_E = 0.081819221456
 r_E_km = 6378.1363
 
 # Helper functions
 def calc_denom(ecc, lat_rad):
-    return math.sqrt(1.0 - ecc**2 * (math.sin(lat_rad**2))
+    return math.sqrt(1.0 - ecc**2 * math.pow(math.sin(lat_rad), 2.0))
 
 # Check and parse script arguments
 if len(sys.argv) == 4:
@@ -32,17 +34,19 @@ if len(sys.argv) == 4:
         lat_deg = float(sys.argv[1])
         lon_deg = float(sys.argv[2])
         hae_km = float(sys.argv[3])
-    
+    except ValueError:
+        print('Error: Latitude, longitude, and altitude must be numbers.')
+        sys.exit()
 else:
     print('Usage: python3 llh_to_ecef.py lat_deg lon_deg hae_km')
     sys.exit()
 
 # Calculate ECEF coordinates
-lat_rad = lat_deg * math.pi / 180.0 #converts latitude degrees to radians
-lon_rad = lon_deg * math.pi / 180.0 #converts longitude degrees to radians
-denom = calc_denom(E_E, lat_rad)
-	C_E = r_E_km / denom
-	S_E = (r_E_km * (1 - E_E**2)) / denom
+lat_rad = lat_deg * math.pi / 180.0 #converts lattitude from degrees to radians
+lon_rad = lon_deg * math.pi / 180.0 #converts longitutde from degrees to radians
+denom = calc_denom(e_E, lat_rad)
+C_E = r_E_km / denom
+S_E = (r_E_km * (1 - e_E**2)) / denom
 r_x = (C_E + hae_km) * math.cos(lat_rad) * math.cos(lon_rad)
 r_y = (C_E + hae_km) * math.cos(lat_rad) * math.sin(lon_rad)
 r_z = (S_E +hae_km) * math.sin(lat_rad)
